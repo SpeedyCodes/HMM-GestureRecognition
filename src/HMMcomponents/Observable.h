5bd96c23c21714b24f58d954d7eb3468699b52ac
Jesse Daems <42868171+SpeedyCodes@users.noreply.github.com>
2023-04-28 13:13:00 +0200

    Hmm class geïmplementeerd (#2)
    * basis HMM klasse geÏmplementeerd
    
    Co-authored-by: BeastzRage <marouan.elmarnissi@hotmail.com>

---
+++ src/HMMcomponents/Observable.h
@@ -0,0 +1,11 @@
+#ifndef HMM_GESTURERECOGNITION_OBSERVABLE_H
+#define HMM_GESTURERECOGNITION_OBSERVABLE_H
+
+class Observable {
+public:
+    int id;
+
+    explicit Observable(int id) : id(id) {}
+};
+
+#endif //HMM_GESTURERECOGNITION_OBSERVABLE_H
