05f7b5af1f18c8e215e1f51e928e13d9b39d085a
darik0d <teleginaaleksandra1@gmail.com>
2023-05-23 16:28:14 +0200

    Final evaluate (and improve) performance
    ... of isolated gestures from video

--- tests/evaluatePerformance.cpp
+++ tests/evaluatePerformance.cpp
@@ -4,55 +4,36 @@
 #include "src/GestureLibrary.h"
 #include "src/utils/MediapipeInterface.h"
 #include <iostream>
-//#include <filesystem>
-#include <QDir>
-//#include <windows.h>
+#include <filesystem>
 
 
-//void evaluateRecognizeGesture(GestureLibrary& library, std::string pathFolderToEvaluate){
-//    MediapipeInterface* minterface = new MediapipeInterface;
-//    unsigned int allGoods = 0;
-//    unsigned int allTries = 0;
-//    for(std::pair<std::string, Gesture> gestureTuple:library.getGestures()){
-//        std::string gestureName = gestureTuple.first;
-//        Gesture gesture = gestureTuple.second;
-//        const char* gestureFolder = (pathFolderToEvaluate + "/" + gestureName).c_str();
-//        unsigned int gestureSucces = 0;
-//        unsigned int gestureAll = 0;
-////        WIN32_FIND_DATAA fileData;
-////        HANDLE hFind = FindFirstFileA(gestureFolder, &fileData);
-////
-////        if (hFind != INVALID_HANDLE_VALUE) {
-////            do {
-////                if ((fileData.dwFileAttributes & FILE_ATTRIBUTE_DIRECTORY) == 0) {
-////                    std::cout << fileData.cFileName << std::endl;
-////                    // Perform operations on the file here
-////                    const char* path = reinterpret_cast<const char *>(fileData.cFileName);
-////                    std::string predictedGesture = library.recognizeFromVideo(path, minterface);
-////                    if(predictedGesture == gestureName) gestureSucces++;
-////                    gestureAll++;
-////                }
-////            } while (FindNextFileA(hFind, &fileData));
-////
-////            FindClose(hFind);
-////        }
-//        for (const auto& entry : filesystem::directory_iterator(gestureFolder)) {
-//            if (entry.is_regular_file()) {
-//                std::cout << entry.path() << std::endl;
-//                const char* path = reinterpret_cast<const char *>(entry.path().c_str());
-//                std::string predictedGesture = library.recognizeFromVideo(path, minterface);
-//                if(predictedGesture == gestureName) gestureSucces++;
-//                gestureAll++;
-//            }
-//        }
-//        std::cerr << "Result for " << gestureName << " = " << gestureSucces << "/" << gestureAll << std::endl;
-//        allGoods += gestureSucces;
-//        allTries += gestureAll;
-//    }
-//    std::cerr << "Result for the whole library is " << allGoods << "/" << allTries << std::endl;
-//    std::cout << "Congratulation, it's " << allGoods*100/allTries << "%" << std::endl;
-//    delete minterface;
-//}
+void evaluateRecognizeGesture(GestureLibrary& library, std::string pathFolderToEvaluate){
+    MediapipeInterface* minterface = new MediapipeInterface;
+    unsigned int allGoods = 0;
+    unsigned int allTries = 0;
+    for(std::pair<std::string, Gesture> gestureTuple:library.getGestures()){
+        std::string gestureName = gestureTuple.first;
+        Gesture gesture = gestureTuple.second;
+        const char* gestureFolder = (pathFolderToEvaluate + "/" + gestureName).c_str();
+        unsigned int gestureSucces = 0;
+        unsigned int gestureAll = 0;
+        for (const auto& entry : filesystem::directory_iterator(gestureFolder)) {
+            if (entry.is_regular_file()) {
+                std::cout << entry.path() << std::endl;
+                const char* path = reinterpret_cast<const char *>(entry.path().c_str());
+                std::string predictedGesture = library.recognizeFromVideo(path, minterface);
+                if(predictedGesture == gestureName) gestureSucces++;
+                gestureAll++;
+            }
+        }
+        std::cerr << "Result for " << gestureName << " = " << gestureSucces << "/" << gestureAll << std::endl;
+        allGoods += gestureSucces;
+        allTries += gestureAll;
+    }
+    std::cerr << "Result for the whole library is " << allGoods << "/" << allTries << std::endl;
+    std::cout << "Congratulation, it's " << allGoods*100.0/allTries << "%" << std::endl;
+    delete minterface;
+}
 void alt_evaluateRecognizeGesture(GestureLibrary& library, const std::vector<std::string>& videosToEvaluate){
     MediapipeInterface* minterface = new MediapipeInterface;
     unsigned int allGoods = 0;
@@ -73,45 +54,48 @@ void alt_evaluateRecognizeGesture(GestureLibrary& library, const std::vector<std
         allGoods += gestureSucces;
         allTries += gestureAll;
     }
+    std::cerr << "Result for the whole library is " << allGoods << "/" << allTries << std::endl;
+    std::cout << "Congratulation, it's " << allGoods*100.0/allTries << "%" << std::endl;
+    delete minterface;
 }
 
 int main(){
     std::string libPath = "tests/182/182.gesturelibrary";
     GestureLibrary library(libPath);
-    // Set pathtoFolder
     std::string path = R"(C:\Users\dasha\PycharmProjects\HMM-GestureRecognition-Collect-data\HMM-GestureRecognition-Collect-data\Collected_videos)";
-    // start evaluateRecognizeGesture
-    alt_evaluateRecognizeGesture(library, {"C:/Users/dasha/PycharmProjects/HMM-GestureRecognition-Collect-data/HMM-GestureRecognition-Collect-data/Collected_videos/eight/eight0_0.avi",
-                                           "C:/Users/dasha/PycharmProjects/HMM-GestureRecognition-Collect-data/HMM-GestureRecognition-Collect-data/Collected_videos/eight/eight1_0.avi",
-                                           "C:/Users/dasha/PycharmProjects/HMM-GestureRecognition-Collect-data/HMM-GestureRecognition-Collect-data/Collected_videos/eight/eight2_0.avi",
-                                           "C:/Users/dasha/PycharmProjects/HMM-GestureRecognition-Collect-data/HMM-GestureRecognition-Collect-data/Collected_videos/eight/eight3_0.avi",
-                                           "C:/Users/dasha/PycharmProjects/HMM-GestureRecognition-Collect-data/HMM-GestureRecognition-Collect-data/Collected_videos/eight/eight4_0.avi",
-                                           "C:/Users/dasha/PycharmProjects/HMM-GestureRecognition-Collect-data/HMM-GestureRecognition-Collect-data/Collected_videos/eight/eight5_0.avi",
-                                           "C:/Users/dasha/PycharmProjects/HMM-GestureRecognition-Collect-data/HMM-GestureRecognition-Collect-data/Collected_videos/eight/eight6_0.avi",
-                                           "C:/Users/dasha/PycharmProjects/HMM-GestureRecognition-Collect-data/HMM-GestureRecognition-Collect-data/Collected_videos/eight/eight7_0.avi",
-                                           "C:/Users/dasha/PycharmProjects/HMM-GestureRecognition-Collect-data/HMM-GestureRecognition-Collect-data/Collected_videos/eight/eight8_0.avi",
-                                           "C:/Users/dasha/PycharmProjects/HMM-GestureRecognition-Collect-data/HMM-GestureRecognition-Collect-data/Collected_videos/eight/eight9_0.avi",
-                                           "C:/Users/dasha/PycharmProjects/HMM-GestureRecognition-Collect-data/HMM-GestureRecognition-Collect-data/Collected_videos/two/two0_0.avi",
-            "C:/Users/dasha/PycharmProjects/HMM-GestureRecognition-Collect-data/HMM-GestureRecognition-Collect-data/Collected_videos/two/two1_0.avi",
-            "C:/Users/dasha/PycharmProjects/HMM-GestureRecognition-Collect-data/HMM-GestureRecognition-Collect-data/Collected_videos/two/two2_0.avi",
-            "C:/Users/dasha/PycharmProjects/HMM-GestureRecognition-Collect-data/HMM-GestureRecognition-Collect-data/Collected_videos/two/two3_0.avi",
-            "C:/Users/dasha/PycharmProjects/HMM-GestureRecognition-Collect-data/HMM-GestureRecognition-Collect-data/Collected_videos/two/two4_0.avi",
-            "C:/Users/dasha/PycharmProjects/HMM-GestureRecognition-Collect-data/HMM-GestureRecognition-Collect-data/Collected_videos/two/two5_0.avi",
-            "C:/Users/dasha/PycharmProjects/HMM-GestureRecognition-Collect-data/HMM-GestureRecognition-Collect-data/Collected_videos/two/two6_0.avi",
-            "C:/Users/dasha/PycharmProjects/HMM-GestureRecognition-Collect-data/HMM-GestureRecognition-Collect-data/Collected_videos/two/two7_0.avi",
-            "C:/Users/dasha/PycharmProjects/HMM-GestureRecognition-Collect-data/HMM-GestureRecognition-Collect-data/Collected_videos/two/two8_0.avi",
-            "C:/Users/dasha/PycharmProjects/HMM-GestureRecognition-Collect-data/HMM-GestureRecognition-Collect-data/Collected_videos/two/two9_0.avi",
-                                           "C:/Users/dasha/PycharmProjects/HMM-GestureRecognition-Collect-data/HMM-GestureRecognition-Collect-data/Collected_videos/one/one0_0.avi",
-                                           "C:/Users/dasha/PycharmProjects/HMM-GestureRecognition-Collect-data/HMM-GestureRecognition-Collect-data/Collected_videos/one/one1_0.avi",
-                                           "C:/Users/dasha/PycharmProjects/HMM-GestureRecognition-Collect-data/HMM-GestureRecognition-Collect-data/Collected_videos/one/one2_0.avi",
-                                           "C:/Users/dasha/PycharmProjects/HMM-GestureRecognition-Collect-data/HMM-GestureRecognition-Collect-data/Collected_videos/one/one3_0.avi",
-                                           "C:/Users/dasha/PycharmProjects/HMM-GestureRecognition-Collect-data/HMM-GestureRecognition-Collect-data/Collected_videos/one/one4_0.avi",
-                                           "C:/Users/dasha/PycharmProjects/HMM-GestureRecognition-Collect-data/HMM-GestureRecognition-Collect-data/Collected_videos/one/one5_0.avi",
-                                           "C:/Users/dasha/PycharmProjects/HMM-GestureRecognition-Collect-data/HMM-GestureRecognition-Collect-data/Collected_videos/one/one6_0.avi",
-                                           "C:/Users/dasha/PycharmProjects/HMM-GestureRecognition-Collect-data/HMM-GestureRecognition-Collect-data/Collected_videos/one/one7_0.avi",
-                                           "C:/Users/dasha/PycharmProjects/HMM-GestureRecognition-Collect-data/HMM-GestureRecognition-Collect-data/Collected_videos/one/one8_0.avi",
-                                           "C:/Users/dasha/PycharmProjects/HMM-GestureRecognition-Collect-data/HMM-GestureRecognition-Collect-data/Collected_videos/one/one9_0.avi"
-                                 }
-    );
+    evaluateRecognizeGesture(library, "C:/Users/dasha/PycharmProjects/HMM-GestureRecognition-Collect-data/HMM-GestureRecognition-Collect-data/Collected_videos/");
+
+//    alt_evaluateRecognizeGesture(library, {"C:/Users/dasha/PycharmProjects/HMM-GestureRecognition-Collect-data/HMM-GestureRecognition-Collect-data/Collected_videos/eight/eight0_0.avi",
+//                                           "C:/Users/dasha/PycharmProjects/HMM-GestureRecognition-Collect-data/HMM-GestureRecognition-Collect-data/Collected_videos/eight/eight1_0.avi",
+//                                           "C:/Users/dasha/PycharmProjects/HMM-GestureRecognition-Collect-data/HMM-GestureRecognition-Collect-data/Collected_videos/eight/eight2_0.avi",
+//                                           "C:/Users/dasha/PycharmProjects/HMM-GestureRecognition-Collect-data/HMM-GestureRecognition-Collect-data/Collected_videos/eight/eight3_0.avi",
+//                                           "C:/Users/dasha/PycharmProjects/HMM-GestureRecognition-Collect-data/HMM-GestureRecognition-Collect-data/Collected_videos/eight/eight4_0.avi",
+//                                           "C:/Users/dasha/PycharmProjects/HMM-GestureRecognition-Collect-data/HMM-GestureRecognition-Collect-data/Collected_videos/eight/eight5_0.avi",
+//                                           "C:/Users/dasha/PycharmProjects/HMM-GestureRecognition-Collect-data/HMM-GestureRecognition-Collect-data/Collected_videos/eight/eight6_0.avi",
+//                                           "C:/Users/dasha/PycharmProjects/HMM-GestureRecognition-Collect-data/HMM-GestureRecognition-Collect-data/Collected_videos/eight/eight7_0.avi",
+//                                           "C:/Users/dasha/PycharmProjects/HMM-GestureRecognition-Collect-data/HMM-GestureRecognition-Collect-data/Collected_videos/eight/eight8_0.avi",
+//                                           "C:/Users/dasha/PycharmProjects/HMM-GestureRecognition-Collect-data/HMM-GestureRecognition-Collect-data/Collected_videos/eight/eight9_0.avi",
+//                                           "C:/Users/dasha/PycharmProjects/HMM-GestureRecognition-Collect-data/HMM-GestureRecognition-Collect-data/Collected_videos/two/two0_0.avi",
+//            "C:/Users/dasha/PycharmProjects/HMM-GestureRecognition-Collect-data/HMM-GestureRecognition-Collect-data/Collected_videos/two/two1_0.avi",
+//            "C:/Users/dasha/PycharmProjects/HMM-GestureRecognition-Collect-data/HMM-GestureRecognition-Collect-data/Collected_videos/two/two2_0.avi",
+//            "C:/Users/dasha/PycharmProjects/HMM-GestureRecognition-Collect-data/HMM-GestureRecognition-Collect-data/Collected_videos/two/two3_0.avi",
+//            "C:/Users/dasha/PycharmProjects/HMM-GestureRecognition-Collect-data/HMM-GestureRecognition-Collect-data/Collected_videos/two/two4_0.avi",
+//            "C:/Users/dasha/PycharmProjects/HMM-GestureRecognition-Collect-data/HMM-GestureRecognition-Collect-data/Collected_videos/two/two5_0.avi",
+//            "C:/Users/dasha/PycharmProjects/HMM-GestureRecognition-Collect-data/HMM-GestureRecognition-Collect-data/Collected_videos/two/two6_0.avi",
+//            "C:/Users/dasha/PycharmProjects/HMM-GestureRecognition-Collect-data/HMM-GestureRecognition-Collect-data/Collected_videos/two/two7_0.avi",
+//            "C:/Users/dasha/PycharmProjects/HMM-GestureRecognition-Collect-data/HMM-GestureRecognition-Collect-data/Collected_videos/two/two8_0.avi",
+//            "C:/Users/dasha/PycharmProjects/HMM-GestureRecognition-Collect-data/HMM-GestureRecognition-Collect-data/Collected_videos/two/two9_0.avi",
+//                                           "C:/Users/dasha/PycharmProjects/HMM-GestureRecognition-Collect-data/HMM-GestureRecognition-Collect-data/Collected_videos/one/one0_0.avi",
+//                                           "C:/Users/dasha/PycharmProjects/HMM-GestureRecognition-Collect-data/HMM-GestureRecognition-Collect-data/Collected_videos/one/one1_0.avi",
+//                                           "C:/Users/dasha/PycharmProjects/HMM-GestureRecognition-Collect-data/HMM-GestureRecognition-Collect-data/Collected_videos/one/one2_0.avi",
+//                                           "C:/Users/dasha/PycharmProjects/HMM-GestureRecognition-Collect-data/HMM-GestureRecognition-Collect-data/Collected_videos/one/one3_0.avi",
+//                                           "C:/Users/dasha/PycharmProjects/HMM-GestureRecognition-Collect-data/HMM-GestureRecognition-Collect-data/Collected_videos/one/one4_0.avi",
+//                                           "C:/Users/dasha/PycharmProjects/HMM-GestureRecognition-Collect-data/HMM-GestureRecognition-Collect-data/Collected_videos/one/one5_0.avi",
+//                                           "C:/Users/dasha/PycharmProjects/HMM-GestureRecognition-Collect-data/HMM-GestureRecognition-Collect-data/Collected_videos/one/one6_0.avi",
+//                                           "C:/Users/dasha/PycharmProjects/HMM-GestureRecognition-Collect-data/HMM-GestureRecognition-Collect-data/Collected_videos/one/one7_0.avi",
+//                                           "C:/Users/dasha/PycharmProjects/HMM-GestureRecognition-Collect-data/HMM-GestureRecognition-Collect-data/Collected_videos/one/one8_0.avi",
+//                                           "C:/Users/dasha/PycharmProjects/HMM-GestureRecognition-Collect-data/HMM-GestureRecognition-Collect-data/Collected_videos/one/one9_0.avi"
+//                                 }
+//    );
     return 0;
 }
