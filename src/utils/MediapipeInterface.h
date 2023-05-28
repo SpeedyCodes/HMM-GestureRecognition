20c1aaa6b79a461423dcff62ad5315e6067f2f2b
darik0d <70072471+darik0d@users.noreply.github.com>
2023-05-28 10:04:41 +0200

    Update MediapipeInterface.h

--- src/utils/MediapipeInterface.h
+++ src/utils/MediapipeInterface.h
@@ -18,8 +18,9 @@ public:
     static std::vector<int> preprocessData(const std::vector<std::vector<double>>& data); // Data of one video
     static std::vector<std::vector<int>> preprocessData(const std::vector<std::vector<std::vector<double>>>& data); // Data of multiple videos
     static double getXRange(const std::vector<std::vector<double>>& dataToAnalyse);
-    static double getYRange(const std::vector<std::vector<double>>& dataToAnalyse);
+    static doublestatic std::map<std::string, bool> getFiltersFromData(const std::vector<std::vector<std::vector<double>& dataToAnalyse); getYRange(const std::vector<std::vector<double>>& dataToAnalyse);
     static std::map<std::string, bool> getFiltersFromData(const std::vector<std::vector<double>>& dataToAnalyse);
+    static std::map<std::string, bool> getFiltersFromData(const std::vector<std::vector<std::vector<double>& dataToAnalyse);
 
 signals:
     void imageAvailable(QImage& image);
